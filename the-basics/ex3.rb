d342bb30b9196e739d14104f13225ee66f7fb9fb
Mitchell Anderson <mitch.and03@gmail.com>
2019-02-18 18:12:41 -0600

    Deleting old the-basicsfolder.

--- the-basics/ex3.rb
+++
@@ -1,9 +0,0 @@
-movies = {
-  pacific_rim: 2013,
-  up: 2009,
-  wall_e: 2008,
-  tron_legacy: 2010,
-  the_secret_life_of_walter_mitty: 2013
-}
-
-movies.each { |movie, year|  puts year}
--- the-basics/ex7.rb
+++
@@ -1,1 +0,0 @@
-#the error message tells me that instead of a } a ) was used to close a block or other statement.
